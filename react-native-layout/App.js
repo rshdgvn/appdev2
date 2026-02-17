ecc2039c2c03b481ae4918c8397762ca1a443565
rshdgvn <rasheedgavinesponga@gmail.com>
2026-02-18 00:39:41 +0800

    Integrated Login and Signup screens into App.js

--- react-native-layout/App.js
+++ react-native-layout/App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, SafeAreaView } from "react-native";
+import Login from "./src/screens/Login";
+import Signup from "./src/screens/Signup";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Login />
+      <Signup />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
